4315616e9bed549ec1cbb8f5d12b3f5510f9bc5d
Harshvardhan9009 <harshvardhants7799@gmail.com>
2026-07-03 12:55:28 +0530

    feat: initialize backend project with MongoDB configuration and environment variable setup

--- BackEnd/src/config/db.ts
+++ BackEnd/src/config/db.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import { env } from "./env.js";
 
 export const connectDB = async () => {
+    console.log("Mongo URI:", env.mongoUri);
     try {
         await mongoose.connect(env.mongoUri);
         console.log("MongoDB Connected");
